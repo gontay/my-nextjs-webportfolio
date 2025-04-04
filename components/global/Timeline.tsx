2d2b72b541d1caa51265586ca1f199d5d3baae02
gontay <gtkw98@outlook.com>
2025-04-04 21:24:59 +0800

    Updated Timeline

--- components/global/Timeline.tsx
+++ components/global/Timeline.tsx
@@ -8,12 +8,12 @@ const Timeline = (props: Props) => {
     <h1 className="justify-center text-center text-xl font-bold p-5">Professional Timeline</h1>
     <div>
     <ol className="relative border-s border-gray-200 dark:border-gray-700">                  
-        {/* <li className="mb-10 ms-4">
+        <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2024</time>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Graduated from Singapore University of Social Sciences 🎉</h3>
-            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Graduated with a Bachelor of Applied Science in Information Technology and Business with Honors.</p>
-        </li> */}
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 2025</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">ABeam Singapore</h3>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Employed as a SAP FI System Analyst 🤓</p>
+        </li>
         <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2024</time>
