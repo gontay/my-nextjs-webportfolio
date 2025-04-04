e40e4fbf7a94f8e72cc9c94a3013e97ee24d8b7a
gontay <gtkw98@outlook.com>
2025-04-04 23:26:13 +0800

    Added Links in Navbar

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -4,6 +4,7 @@ import { ModeToggle } from '../ld-toggle'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import { SiGitbook } from "react-icons/si";
+import { link } from 'fs'
 
 type Props = {}
 
@@ -11,7 +12,7 @@ const Navbar = (props: Props) => {
   return (
     <div className="sticky top-0 z-10 flex max-w-3xl place-content-between place-items-center p-2 shadow-md rounded-md bg-slate-100 dark:bg-black dark:drop-shadow-glow ">
       <Link href="/">
-        <Image src="/images/light/favicon-32x32.png" alt="image" 
+        <Image src="/images/light/favicon-32x32.png" alt="image"
           width={32}
           height={32}
           className='dark:hidden hover:scale-110'
@@ -22,6 +23,18 @@ const Navbar = (props: Props) => {
           className='hidden dark:block hover:scale-110'
           />
       </Link>
+      <div className='flex justify-items-end'>
+      <Link href="https://github.com/gontay">
+        <Button variant="link" className='dark:hidden font-black'>Projects</Button>
+        <Button variant="linkDark" className='hidden dark:block'>Projects</Button>
+      </Link>
+      </div>
+      <div className='flex justify-items-end'>
+      <Link href="https://gontay.substack.com/">
+        <Button variant="link" className='dark:hidden font-black'>Blogs</Button>
+        <Button variant="linkDark" className='hidden dark:block'>Blogs</Button>
+      </Link>
+      </div>
       <div className='flex gap-x-2'>
         <ModeToggle/>
       </div>
--- components/global/Socials.tsx
+++ components/global/Socials.tsx
@@ -7,7 +7,7 @@ type Props = {}
 const Socials = (props: Props) => {
   return (
     <div className="flex gap-2 justify-center text-center">
-        <Link href="https://www.linkedin.com/in/gerald-tay-kai-wen-089a62134/"><LinkedInLogoIcon className="size-5 hover:scale-125"/></Link>
+        <Link href="https://www.linkedin.com/in/gerald-tay-kai-wen-089a62134/" className='justify-center hover:scale-125'><LinkedInLogoIcon className="size-5 "/></Link>
         <Link href="https://github.com/gontay"><GitHubLogoIcon className="size-5 hover:scale-125"/></Link>
         <Link href="https://www.instagram.com/gontographs/"><InstagramLogoIcon className="size-5 hover:scale-125"/></Link>
     </div>
